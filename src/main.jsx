0cf5ce99374057ff99f75cd9728818ba5c914d78
Damilola91 <damilola.wiwoloku@gmail.com>
2024-11-12 10:43:03 +0100

    modifiche main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,20 +6,17 @@ import { BookContextProvider } from './components/contexts/BookContext.jsx'
 import { ThemeContextProvider } from './components/contexts/ThemeContext.jsx'
 import { CommentSelectedCardProvider } from './components/contexts/CommentSelectedCard.jsx'
 import { CartContextProvider } from './components/contexts/CartContext.jsx'
-import { LoginContextProvider } from './components/contexts/LoginContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <LoginContextProvider>
-            <CartContextProvider>
-                <CommentSelectedCardProvider>
-                    <ThemeContextProvider>
-                        <BookContextProvider>
-                            <App />
-                        </BookContextProvider>
-                    </ThemeContextProvider>
-                </CommentSelectedCardProvider>
-            </CartContextProvider>
-        </LoginContextProvider>
+        <CartContextProvider>
+            <CommentSelectedCardProvider>
+                <ThemeContextProvider>
+                    <BookContextProvider>
+                        <App />
+                    </BookContextProvider>
+                </ThemeContextProvider>
+            </CommentSelectedCardProvider>
+        </CartContextProvider>
     </StrictMode>
 )
